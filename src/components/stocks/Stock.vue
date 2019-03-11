c7bde9957004a6426adb7bcfc78314ef56cbe206
Alexander Melo <alexander.melo@ibm.com>
2019-03-11 13:29:46 -0500

    added the stock component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>The Stock - Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card stock">
+      <div class="card__heading">
+        <h3 class="stock__title">IBM</h3>
+        <span class="stock__price">$112.12</span>
+      </div>
+      <div class="card__body">
+        <form>
+          <div class="stock__input-group">
+            <input type="number" class="stock__input" placeholder="Quantity">
+            <input type="submit" class="stock__button" value="Buy">
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>The Stocks Component</h1>
+  <section class="stocks">
+    <div class="row">
+      <Stock v-for="stock in stocks"/>
+    </div>
+  </section>
 </template>
+
+<script>
+import Stock from "./Stock.vue";
+
+export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: "BMW", price: 110 },
+        { id: 2, name: "Google", price: 200 },
+        { id: 3, name: "Apple", price: 250 },
+        { id: 4, name: "Twitter", price: 80 }
+      ]
+    };
+  },
+  components: {
+    Stock
+  }
+};
+</script>
